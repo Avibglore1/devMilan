567d8e515bac74a1d3da56f2bebeca85876b052e
Avinash <avinashkumarofficial95@gmail.com>
2025-10-06 16:01:51 +0530

    feat: add user feed endpoint to retrieve users excluding connections

--- src/routes/userConnection.js
+++ src/routes/userConnection.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { userAuth } from "../middlewares/userAuth.js";
 import { ConnectionRequest } from "../models/connectionRequest.js";
+import { User } from "../models/user.js";
 export const userRouter = express.Router();
 
 const SAFE_USER_DATA = "firstName lastName photoUrl age gender about skills";
@@ -49,6 +50,43 @@ userRouter.get("/user/connections", userAuth, async(req,res)=>{
     return res.status(200).json({message: data});
     } catch (error) {
         res.status(404).send("message: " + error.message)
+    }
+    
+})
+
+userRouter.get("/user/feed", userAuth, async(req,res)=>{
+    try {
+        const loggedInUser = req.user;
+        const connections = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+
+        const hideUserFromFeed = new Set();
+        connections.forEach((item)=>{
+            hideUserFromFeed.add(item.fromUserId.toString());
+            hideUserFromFeed.add(item.toUserId.toString());
+        });
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page-1)*limit;
+
+        const userToShow = await User.find({
+            $and: [
+                {_id: {$nin: Array.from(hideUserFromFeed)}},
+                {_id: {$ne: loggedInUser._id}}]
+            }
+        )
+        .select(SAFE_USER_DATA)
+        .skip(skip)
+        .limit(limit);
+
+        res.json({message: userToShow});
+    } catch (error) {
+        res.status(404).send('Error: '+ error.message);
     }
     
 })
